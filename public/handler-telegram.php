de935927df073410ba88733c14453b81771af8af
Andrey Piksaev <pikcaev@gmail.com>
2019-10-26 20:21:51 +0300

    Исправлена ошибка с проверкой токена

--- public/handler-telegram.php
+++ public/handler-telegram.php
@@ -5,6 +5,12 @@ use unreal4u\TelegramAPI\Telegram\Types\Update;
 
 require_once 'handler-main.php';
 
+// Проверяем секретный токен
+$explodedURI = explode('/', $_SERVER['REQUEST_URI']);
+if ($explodedURI[count($explodedURI)-1] !== Config::TELEGRAM_API_ACCESS_TOKEN)
+	die();
+unset($explodedURI);
+
 // Декодируем полученное событие из JSON в объект
 $event = json_decode(file_get_contents('php://input'), true);
 
@@ -13,13 +19,6 @@ if (json_last_error() !== JSON_ERROR_NONE)
 
 if (is_null($event))
 	throw new Exception ('Decoded event is null...');
-
-// Проверяем секретный токен
-$explodedURI = explode('/', $_SERVER['REQUEST_URI']);
-if ($explodedURI(count($explodedURI)-1) !== Config::TELEGRAM_API_ACCESS_TOKEN)
-	die();
-
-unset($explodedURI);
 
 // Запускаем бота
 $event = new Update($event);
