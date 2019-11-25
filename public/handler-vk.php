3e3d744e4989bf5134ed9e608f9030997520782a
Andrey Piksaev <pikcaev@gmail.com>
2019-11-25 17:52:30 +0300

    Добавлен завершающий этап логирования

--- public/handler-vk.php
+++ public/handler-vk.php
@@ -20,6 +20,6 @@ $VkBot->handle($event);
 
 // Завершаем логирование
 if (isset($BOT_LOG)) {
-	$BOT_LOG->addToLog('Script time: ' . round(microtime(true) - $START_TIME, 4) . " sec;");
+	$BOT_LOG->addToLog(' - Script time: ' . round(microtime(true) - $START_TIME, 4) . ' sec; Memory usage: ' . memory_get_usage() . ' bytes; Memory peak usage: ' . memory_get_peak_usage() . ' bytes');
 	Logger::log($BOT_LOG->fileName, $BOT_LOG->textLog);
 }
