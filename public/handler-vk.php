3f58a051434a5dfa2431260b220ee5555146ca82
Andrey Piksaev <pikcaev@gmail.com>
2019-11-25 21:54:48 +0300

    Включено логирование времени выполнения скрипта

--- public/handler-vk.php
+++ public/handler-vk.php
@@ -23,3 +23,5 @@ if (isset($BOT_LOG)) {
 	$BOT_LOG->addToLog(' - Script time: ' . round(microtime(true) - $START_TIME, 4) . ' sec; Memory usage: ' . memory_get_usage() . ' bytes; Memory peak usage: ' . memory_get_peak_usage() . ' bytes');
 	Logger::log($BOT_LOG->fileName, $BOT_LOG->textLog);
 }
+
+Logger::logScriptTime(round(microtime(true) - $START_TIME, 4));
