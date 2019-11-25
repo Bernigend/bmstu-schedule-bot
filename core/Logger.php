8b5ae0fd4b44a5f08c4c02929a8cba14b8e32097
Andrey Piksaev <pikcaev@gmail.com>
2019-11-25 17:39:00 +0300

    Добавлена возможность логирования состояния бота

--- core/Logger.php
+++ core/Logger.php
@@ -7,6 +7,39 @@ namespace Core;
 class Logger
 {
 	/**
+	 * Текст, который попадёт в лог
+	 * @var string
+	 */
+	public $textLog;
+
+	/**
+	 * Название файла в каталоге с логами
+	 * @var string
+	 */
+	public $fileName;
+
+	/**
+	 * Logger constructor.
+	 *
+	 * @param string $fileName - название файла в каталоге с логами
+	 */
+	public function __construct(string $fileName)
+	{
+		$this->fileName = $fileName;
+		$this->textLog  = '';
+	}
+
+	/**
+	 * Добавляет в лог необходимые данные
+	 *
+	 * @param string $value - данные для добавления
+	 */
+	public function addToLog(string $value): void
+	{
+		$this->textLog .= $value;
+	}
+
+	/**
 	 * Заносит данные в требуемый лог файл
 	 *
 	 * @param string $fileName - название файла в каталоге с логами
